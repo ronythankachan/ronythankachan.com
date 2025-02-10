0ffa63ff2ccf09e87d5d4ba44eabed95d6fb1c8f
rony thankachan <rony.mail2me@gmail.com>
2025-02-10 12:46:53 +0530

    layout style fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-10`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-4 sm:mx-10`}
       >
         <Navbar />
         {children}
