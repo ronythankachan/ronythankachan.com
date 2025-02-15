4aa8947877a95d90dd6417189232309681e5c945
rony thankachan <rony.mail2me@gmail.com>
2025-02-15 16:42:30 +0530

    Subscribe api integrated for newsletter

---
+++ app/api/newsletter/subscribe/route.ts
@@ -0,0 +1,72 @@
+import { NextResponse } from 'next/server';
+
+const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const createErrorResponse = (message: string, status: number) => {
+  return NextResponse.json({ error: message }, { status });
+}
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  if (!email) {
+    return createErrorResponse("Email is required", 400);
+  }
+
+  if (!isValidEmail(email)) {
+    return createErrorResponse("Invalid email format", 400);
+  }
+
+  try {
+    // Fetch the group ID
+    const groupResponse = await fetch('https://connect.mailerlite.com/api/groups', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${process.env.MAILERLITE_API_KEY}`,
+      },
+    });
+
+    if (!groupResponse.ok) {
+      const errorData = await groupResponse.json();
+      console.error('Group API Error:', errorData); // Log the actual error
+      return createErrorResponse('Subscription failed', 500);
+    }
+
+    const groupData = await groupResponse.json();
+    const group = groupData.data.find((g: any) => g.name === "Newsletter");
+
+    if (!group) {
+      console.error('Group not found for Newsletter');
+      return createErrorResponse('Subscription failed', 500);
+    }
+
+    const groupId = group.id;
+
+    // Subscribe the email to the group
+    const response = await fetch('https://connect.mailerlite.com/api/subscribers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.MAILERLITE_API_KEY}`,
+      },
+      body: JSON.stringify({
+        email: email,
+        groups: [groupId]
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error('Subscription API Error:', errorData); // Log the actual error
+      return createErrorResponse('Subscription failed', 500);
+    }
+
+    return NextResponse.json({ message: `Thank you for subscribing` });
+  } catch (error) {
+    console.error('Unexpected Error:', error); // Log unexpected errors
+    return createErrorResponse('Subscription failed', 500);
+  }
+}
