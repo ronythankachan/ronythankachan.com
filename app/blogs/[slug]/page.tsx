0e81499f0a25d2a0c7b3cdb9ffd7820086afeec7
rony thankachan <rony.mail2me@gmail.com>
2025-02-16 22:35:33 +0530

    Blogpage error fix

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { notFound } from 'next/navigation';
-import BlogPost from '../../components/BlogPost';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { notFound } from "next/navigation";
+import BlogPost from "../../components/BlogPost";
 
 interface BlogPostProps {
   params: {
@@ -12,17 +12,17 @@ interface BlogPostProps {
 }
 
 const BlogPage: React.FC<BlogPostProps> = async ({ params }) => {
-  const { slug } = await params;
-  const filePath = path.join('public', 'blogs', `${slug}.md`);
+  const { slug } = params;
+  const filePath = path.join("public", "blogs", `${slug}.md`);
 
   if (!fs.existsSync(filePath)) {
     notFound();
   }
 
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const fileContent = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContent);
 
   return <BlogPost title={data.title} date={data.date} content={content} />;
 };
 
-export default BlogPage; 
+export default BlogPage;
