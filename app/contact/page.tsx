e863bb1573289c864f7a099bba8d031b0d5e458b
rony thankachan <rony.mail2me@gmail.com>
2025-02-24 23:26:24 +0530

    Contact page init

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "../components/shared/HeroSection";
+import Container from "../components/shared/Container";
+
+const Contact = () => {
+  return (
+    <HeroSection>
+      <Container>Contact me</Container>
+    </HeroSection>
+  );
+};
+
+export default Contact;
