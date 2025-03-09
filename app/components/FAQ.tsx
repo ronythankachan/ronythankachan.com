7113bf143fa52af6e088f96efd67ed8c8cb65b64
rony thankachan <rony.mail2me@gmail.com>
2025-03-09 21:24:43 +0530

    Content update

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -21,22 +21,22 @@ const FAQ = () => {
         "I love hiking, reading, and coding. Sometimes I go for a swim too.",
     },
     {
-      question: "Who influenced you to be a software engineer?",
+      question: "Who influenced you to be a Software Engineer?",
       answer:
-        "My cousin, who was the cool guy with a computer science degree, inspired me. I spent my teenage summers glued to his laptop, dreaming of the day I'd have my own. Spoiler: I made it.",
+        "My cousin, who was the cool guy with a laptop studying Computer Science, inspired me. I spent my teenage summers glued to his laptop, dreaming of the day I'd have my own. Spoiler: I got two laptops now.",
     },
     {
-      question: "Why do you want to be a software engineer?",
+      question: "Why do you want to be a Software Engineer?",
       answer:
-        "I've always wanted to be a software engineer, even before I knew about the perks. Watching people type furiously in movies fascinated me. I knew this was my calling, and I'm still passionate about it—paycheck or not.",
+        "I've always wanted to be a Software Engineer, even before I knew about the perks. Watching people type furiously in movies fascinated me. Computers fascinated me, and I knew this was my calling, and I'm passionate about it—paycheck or not.",
     },
     {
       question: "Do you have a favorite programming language?",
       answer:
-        "I don't. Being a software engineer is like being a chef—you use the best ingredients for the dish. Each language or tool has its purpose, you learn to use them when you need to.",
+        "I don't. Being a Software Engineer is like being a chef—you use the best ingredients for the dish. Each language or tool has its purpose, you learn to use them when you need to.",
     },
     {
-      question: "What is an underrated skill in software engineering?",
+      question: "What is an underrated skill in Software Engineering?",
       answer:
         "Self-learning, hands down. In this field, if you're not learning, you're falling behind. It's like a never-ending game of catch-up, but that's what keeps it exciting!",
     },
