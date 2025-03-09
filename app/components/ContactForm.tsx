155c2f08760e43380cf7c166b50a24b50ecda890
rony thankachan <rony.mail2me@gmail.com>
2025-03-09 19:09:30 +0530

    FAQ section

---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [message, setMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      await response.json();
+      setMessage(
+        "Thank you for your message. I will get back to you as soon as possible."
+      );
+      setIsSuccess(true);
+      setFormData({ name: "", email: "", message: "" });
+    } catch (error) {
+      console.error("Error sending message:", error);
+      setMessage("An error occurred. Please try again.");
+      setIsSuccess(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form
+      className="space-y-4 shadow-lg px-6 md:px-32 py-12 rounded-3xl mt-8 mx-auto max-w-3xl bg-white"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col items-center justify-center gap-2 mb-6">
+        <Image
+          src="/rony2.jpeg"
+          alt="Rony's Picture"
+          width={80}
+          height={80}
+          className="rounded-full"
+        />
+        <h1 className="text-3xl md:text-4xl font-serif text-gray-800">
+          Contact Me
+        </h1>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+          className="flex-1 px-4 py-2 bg-gray-100 rounded-full outline-none w-full focus:ring-2 focus:ring-sky-100"
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          className="flex-1 px-4 py-2 bg-gray-100 rounded-full outline-none w-full focus:ring-2 focus:ring-sky-100"
+          required
+        />
+      </div>
+      <div>
+        <textarea
+          id="message"
+          name="message"
+          rows={4}
+          placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
+          className="flex-1 px-4 py-2 bg-gray-100 rounded-3xl outline-none w-full focus:ring-2 focus:ring-sky-100"
+          required
+        />
+      </div>
+      {message && (
+        <p
+          className={`text-sm mt-2 p-2 rounded-lg ${
+            isSuccess
+              ? "bg-green-100 text-green-700"
+              : "bg-red-100 text-red-700"
+          }`}
+        >
+          {message}
+        </p>
+      )}
+      <div>
+        <button
+          type="submit"
+          className="w-full px-6 py-2 bg-gradient-to-r from-sky-400 to-sky-600 text-white rounded-full font-medium hover:from-sky-500 hover:to-sky-700 transition-colors flex items-center justify-center"
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 00-8 8z"
+              ></path>
+            </svg>
+          ) : (
+            "Submit"
+          )}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
---
+++ app/components/FAQ.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
+const FAQ = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleAccordion = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const faqs = [
+    {
+      question: "What gear do you use for personal use?",
+      answer:
+        "I use a MacBook Air M1 from 2020, still works like a beast except I can't run the fastest deepseek model. My Keychron K4 mechanical keyboard with brown switches makes me feel like a professional typist, and my MX Master 2S mouse so that I can scroll 5000 pages in one second. Oh, and I have a Rode NT USB Mini mic because my voice deserves to be heard in HD.",
+    },
+    {
+      question: "What are your hobbies?",
+      answer:
+        "I love hiking, reading, and coding. Sometimes I go for a swim too.",
+    },
+    {
+      question: "Who influenced you to be a software engineer?",
+      answer:
+        "My cousin, who was the cool guy with a computer science degree, inspired me. I spent my teenage summers glued to his laptop, dreaming of the day I'd have my own. Spoiler: I made it.",
+    },
+    {
+      question: "Why do you want to be a software engineer?",
+      answer:
+        "I've always wanted to be a software engineer, even before I knew about the perks. Watching people type furiously in movies fascinated me. I knew this was my calling, and I'm still passionate about it—paycheck or not.",
+    },
+    {
+      question: "Do you have a favorite programming language?",
+      answer:
+        "I don't. Being a software engineer is like being a chef—you use the best ingredients for the dish. Each language or tool has its purpose, you learn to use them when you need to.",
+    },
+    {
+      question: "What is an underrated skill in software engineering?",
+      answer:
+        "Self-learning, hands down. In this field, if you're not learning, you're falling behind. It's like a never-ending game of catch-up, but that's what keeps it exciting!",
+    },
+  ];
+
+  return (
+    <div className="mt-12">
+      <h1 className="text-[60px] leading-tight font-serif mb-12 text-center">
+        Some FAQs
+      </h1>
+      <div className="max-w-2xl mx-auto">
+        {faqs.map((faq, index) => (
+          <div key={index} className="mb-4">
+            <button
+              onClick={() => toggleAccordion(index)}
+              className={`w-full flex justify-between items-center text-left bg-grayBackground p-8 shadow-sm focus:outline-none ${
+                activeIndex === index
+                  ? "rounded-t-3xl transition-all duration-500 ease-in-out"
+                  : "rounded-3xl transition-all duration-500 ease-in-out"
+              }`}
+            >
+              <span className="font-medium">{faq.question}</span>
+              <ChevronDownIcon
+                className={`h-5 w-5 transform transition-transform ${
+                  activeIndex === index ? "rotate-180" : ""
+                }`}
+              />
+            </button>
+            <div
+              className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                activeIndex === index ? "max-h-40" : "max-h-0"
+              }`}
+            >
+              <div className="px-8 pb-8 rounded-b-3xl bg-grayBackground font-medium">
+                <p>{faq.answer}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
